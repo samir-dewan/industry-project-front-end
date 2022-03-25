3a63d0b7fc13a2698c061aa279413b684b859fba
Ben Thorne <benjaminjamesthorne@gmail.com>
2022-03-25 12:09:36 +0000

    Create itemdetails form

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -1,1 +1,32 @@
+import React from "react";
+import "../ItemDetails/item-details.scss";
 
+export default function ItemDetails() {
+  return (
+    <div className="item-details">
+      <h1>Enter Item Details</h1>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <p>HARD CODE IMG FROM PREVIOUS PAGE</p>
+      <form className="item-details__form">
+        <input
+          placeholder="My favourite Jumper!"
+          className="item-details__form-name"
+          type="text"
+        />
+        <select className="item-details__colours">
+          <option value="whites">Whites</option>
+          <option value="darks">Darks</option>
+          <option value="lights">Lights</option>
+        </select>
+        <select className="item-details__fabric">
+          <option value="synthetics">Synthetics</option>
+          <option value="cotton">Cotton</option>
+          <option value="delicates">Delicates</option>
+        </select>
+        <button>CONFIRM</button>
+      </form>
+    </div>
+  );
+}
