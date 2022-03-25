d1427dff3c52d6cf08ffd0484f766e9c05327f49
samirdewan1995 <samirdewan1995@gmail.com>
2022-03-25 10:05:15 +0000

    add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-/** @format */
 
 //IMPORT MODULES
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
 //IMPORT COMPONENTS
-
+import BasketPage from "./components/BasketPage/BasketPage";
+import Camera from "./components/Camera/Camera";
+import ItemDetails from "./components/ItemDetails/ItemDetails";
+import Scanner from "./components/Scanner/Scanner";
+import Login from "./components/Login/Login";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
     <>
       <Header />
       <Switch>
-        <Redirect from="/home" exact to="/" />
+        <Redirect from="/" exact to="/home" />
         <Route
-          path="/"
+          path="/login"
           exact
-          render={(routerProps) => <Homepage {...routerProps} />}
+          render={(routerProps) => <Login {...routerProps} />}
         />
+        <Route
+          path="/home"
+          exact
+          render={(routerProps) => <BasketPage {...routerProps} />}
+        />
+        <Route
+          path="/scan"
+          exact
+          render={(routerProps) => <Scanner {...routerProps} />}
+          />
+        <Route
+          path="/scan/details"
+          exact
+          render={(routerProps) => <ItemDetails {...routerProps} />}
+          />
+        <Route
+          path="/photo"
+          exact
+          render={(routerProps) => <Camera {...routerProps} />}
+          />
       </Switch>
       <Footer />
     </>
