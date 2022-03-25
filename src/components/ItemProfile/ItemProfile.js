2a1e2da0d83961816d1c70d2cfcf0d55eba7472d
Ben Thorne <benjaminjamesthorne@gmail.com>
2022-03-25 13:38:21 +0000

    Create ItemProfile page

---
+++ src/components/ItemProfile/ItemProfile.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function ItemProfile() {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+        <p>HARD CODE THIS IMAGE</p>
+      </div>
+      <h1>NAME OF YOUR ITEM</h1>
+      <ul>
+        <li>FABRIC TYPE</li>
+        <li>COLOUR</li>
+      </ul>
+      <div>LABELS</div>
+      <div>ADDITIONAL INFO</div>
+    </div>
+  );
+}
